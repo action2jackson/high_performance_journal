d97b2cabac7d1a8458d2853bb6d76bff25b65e3e
Jackson <2action2jackson2@gmail.com>
2020-03-21 19:14:53 -0500

    Redesigned page and added movement (almost complete)

--- journal/static/js/javascript.js
+++ journal/static/js/javascript.js
@@ -5,3 +5,24 @@ const links = document.querySelectorAll(".nav-link");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
+
+/*
+<---DREAMS--->
+document.addEventListener("mousemove", function(e) {
+  var body = document.querySelector('body');
+  var stars = document.createElement('span');
+  stars.className += "stars";
+  var x = e.offsetX;
+  var y = e.offsetY;
+  stars.style.left = x + 'px';
+  stars.style.top = y + 'px';
+  var size = Math.random() * 40;
+  stars.style.width = size + 'px';
+  stars.style.height = size + 'px';
+  body.appendChild(stars);
+
+  setTimeout(function(){
+      stars.remove();
+  },4000)
+}) 
+*/
