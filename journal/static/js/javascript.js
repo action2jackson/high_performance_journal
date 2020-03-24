b8e69b6ab99487dd5476ad7460cf68a98e42ba25
Jackson <2action2jackson2@gmail.com>
2020-03-24 02:45:24 -0500

    Responsive home page (No Comments)

--- journal/static/js/javascript.js
+++ journal/static/js/javascript.js
@@ -1,28 +1,23 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".navigation_container");
-const links = document.querySelectorAll(".nav-link");
+const headerSlide = () => {
+  const burger = document.querySelector('.burger');
+  const header = document.querySelector('.navigation_container');
+  const nav_link = document.querySelectorAll('.nav-link');
+  const stars = document.querySelector('.stars');
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
+  burger.addEventListener('click', () => {
+    header.classList.toggle('header-active');
 
-/*
-<---DREAMS--->
-document.addEventListener("mousemove", function(e) {
-  var body = document.querySelector('body');
-  var stars = document.createElement('span');
-  stars.className += "stars";
-  var x = e.offsetX;
-  var y = e.offsetY;
-  stars.style.left = x + 'px';
-  stars.style.top = y + 'px';
-  var size = Math.random() * 40;
-  stars.style.width = size + 'px';
-  stars.style.height = size + 'px';
-  body.appendChild(stars);
+    
+      nav_link.forEach((link, index) => {
+        if(link.style.animation) {
+          link.style.animation = '';
+        } else {
+          link.style.animation = `headerFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+      });
+    
+    burger.classList.toggle('toggle');
+  });
+}
 
-  setTimeout(function(){
-      stars.remove();
-  },4000)
-}) 
-*/
+headerSlide();
