05c9f4ad06d1533592b30589adeba36b6b4c1db0
Jackson <2action2jackson2@gmail.com>
2020-04-14 14:21:18 -0500

    Added slide show effect to 90 day goal list page

--- journal/static/js/javascript.js
+++ journal/static/js/javascript.js
@@ -46,81 +46,108 @@ document.addEventListener("mousemove", function(e) {
 }) 
 <--- FOR DREAM PAGE --->
 */
-/*
-<--- Save for later --->
+
+
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 var after_header = document.getElementById("AF");
+var goals = document.getElementById("goals");
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function() {
   after_header.style.opacity = 1 - +this.window.pageYOffset/550+'';
   after_header.style.top = +this.window.pageXOffset+'px'
-  if (window.pageYOffset > sticky) {
-    header.classList.remove("header");
-  } else {
-    header.classList.add("header");
-  }
 });
-<--- Save for later --->
-*/
 
 
-var currentStep = 0;
-showStep(currentStep);
+if (window.location.pathname == "/") {
+  var currentStep = 0;
+  showStep(currentStep);
 
-function showStep(step) {
-  var formStep = document.getElementsByClassName("form_step");
-  if (step > 0) {
-    document.body.scrollIntoView(false); 
-  }
-  formStep[step].style.display = "block";
+  function showStep(step) {
+    var formStep = document.getElementsByClassName("form_step");
+    if (step > 0) {
+      document.body.scrollIntoView(false); 
+    }
+    formStep[step].style.display = "block";
 
-  if (step == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-    document.getElementById('nextBtn').style.width = "100%";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
+    if (step == 0) {
+      document.getElementById("prevBtn").style.display = "none";
+      document.getElementById('nextBtn').style.width = "100%";
+    } else {
+      document.getElementById("prevBtn").style.display = "inline";
+    }
 
-  if (step == (formStep.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
-}
-
-function nextPrev(step) {
-  var formStep = document.getElementsByClassName("form_step");
-  if (step == 1 && !validateForm()) return false;
-  formStep[currentStep].style.display = "none";
-  currentStep = currentStep + step;
-  if (currentStep >= formStep.length) {
-    var confirmGoals = confirm("Are you confident with your goals?")
-    if (confirmGoals == true) {
-      document.getElementById("form_for_goals").submit();
-      return false;
+    if (step == (formStep.length - 1)) {
+      document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+      document.getElementById("nextBtn").innerHTML = "Next";
     }
   }
-  showStep(currentStep);
+
+  function nextPrev(step) {
+    var formStep = document.getElementsByClassName("form_step");
+    if (step == 1 && !validateForm()) return false;
+    formStep[currentStep].style.display = "none";
+    currentStep = currentStep + step;
+    if (currentStep >= formStep.length) {
+      var confirmGoals = confirm("Are you confident with your goals?");
+      if (confirmGoals == true) {
+        document.getElementById("form_for_goals").submit();
+        return false;
+      }
+    }
+    showStep(currentStep);
+  }
+
+  nextBtn.addEventListener('click', function() {
+    nextPrev(1)
+  }, false)
+  prevBtn.addEventListener('click', function() {
+    nextPrev(-1)
+  }, false)
+
+
+  function validateForm() {
+    var formStep, emptyInput, i, valid = true;
+    formStep = document.getElementsByClassName("form_step");
+    emptyInput = formStep[currentStep].getElementsByTagName("input");
+    for (i = 0; i < emptyInput.length; i++) {
+      emptyInput[i].addEventListener('input', function(e) {
+        if (emptyInput[i] != "") {
+          this.className = "valid";
+        }
+      });
+      if (emptyInput[i].value == "") {
+        emptyInput[i].className = " invalid";
+        valid = false;
+      }
+    } 
+    return valid;
+  }
 }
 
-nextBtn.addEventListener('click', function() {
-  nextPrev(1)
-}, false)
-prevBtn.addEventListener('click', function() {
-  nextPrev(-1)
-}, false)
 
-function validateForm() {
-  var formStep, emptyInput, i, valid = true;
-  formStep = document.getElementsByClassName("form_step");
-  emptyInput = formStep[currentStep].getElementsByTagName("input");
-  for (i = 0; i < emptyInput.length; i++) {
-    if (emptyInput[i].value == "") {
-      emptyInput[i].className += " invalid";
-      valid = false;
-    } 
+
+if (window.location.pathname == "/goals/") {
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(slide) {
+    showSlides(slideIndex += slide);
   }
-  
-  return valid;
+
+  function showSlides(slide) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (slide > slides.length) {
+      slideIndex = 1;
+    }
+    if (slide < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
 }
