84ef7a92d4035470bd660d169fe3fedc17d829bf
Jackson <2action2jackson2@gmail.com>
2020-04-22 17:16:01 -0500

    Added edit feature to 90 day goals

--- journal/urls.py
+++ journal/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('goals/', views.goals_list, name='goals_list'),
     path('goals/delete/', views.goals_delete, name='goals_delete'),
+    path('goal/edit/<str:pk>/', views.goal_edit, name='goal_edit'),
 ]
